f8a8ff03a22becba8d433c1bd95182b913b7c8bf
robogeek95 <azeezlukman95@gmail.com>
2021-07-24 04:19:01 +0100

    fixed purging dark classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: {
+    enabled: true,
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    options: {
+      safelist: ["dark"],
+    },
+  },
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
