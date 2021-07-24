b71cf5d55650fe207caa6aa30440037e50e7ed02
robogeek95 <azeezlukman95@gmail.com>
2021-07-24 03:59:16 +0100

    fixed project card positioning

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -22,11 +22,11 @@ function ProjectCard({
 }: ProjectCardProps) {
   return (
     <div className="relative pt-12 w-full h-full">
-      <div className="left-16 top-0">
+      <div className="left-16 top-0 -mb-5 sticky z-10">
         <img alt={imageAlt} className="w-full object-cover" src={imageUrl} />
       </div>
 
-      <div className="group relative block pb-10 pt-12 px-8 w-full h-full bg-gray-100 dark:bg-gray-800 rounded-lg md:pb-20 md:px-16">
+      <div className="group relative block pb-10 pt-12 px-8 w-full bg-gray-100 dark:bg-gray-800 rounded-lg md:pb-20 md:px-16">
         <H2>{title}</H2>
         <div className="mt-4 max-w-sm">
           <H2 variant="secondary" className="truncate" as="p">
